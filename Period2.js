94294b6e0b7673ed7a1e50862635f29339e56275
WillVill <bech.william2@gmail.com>
2016-03-04 10:57:24 +0100

    initial

---
+++ Period2.js
@@ -0,0 +1,95 @@
+/**
+ * Created by williambech on 3/3/16.
+ */
+/**
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	PERIOD 2 HAND-IN
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+fd
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	1 - Why would you consider a scripting language as JavaScript as your backend platform?
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+ You would consider a scripting language such as JavaScript as your backend platform when you want a fast and ubiquity
+ backend platform. When writing the backend in javascript you would also have your frontend written in javascript as
+ well this creates ubiquity. Javascript backend platforms are also proven to be faster than other backend platforms.
+
+ Javascript backend platforms also have their disadvantages such as having a less solid foundation compared to making
+ a backend in Java. This leads to very large projects to usually lean towards backend platforms written with Java
+ rather than Javascript
+
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+ 	2 - Explain pros & cons in using Node.js + Express to implement your backend compared
+ 	to a strategy using for example java/jax-rs/tomcat
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+Pros
+ - Ubiquity, frontend and backend written in the same language.
+ - Speed, Node.js is very fast.
+ - Json and database queries uniformity. Sytax is very similar making the memorization of different syntaxes not needed.
+ - Momentum, javascript backends have a lot of momentum, the community is growing and improving much faster than any
+ other competitor.
+
+ Cons
+ - Javascript does not have a solid foundation compared to Java, as it is newer and the way the language has been made.
+ Although this is changing more and more.
+ - The IDEs for javascript are not as intricate as the ones for Java.
+ - Libraries are not as large as the ones for Java.
+
+
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+ 	3 - Explain strategies to implement a Node.js based server architecture that still could
+ 	take advantage of a multi-core server.
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+Node.Js itself is an asynchronous
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	4 - Explain, using relevant examples, concepts related to the testing a REST-API using
+	Node/JavaScript + relevant packages
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	5 - Explain, using relevant examples, the Express concept; Middleware
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+ 	6 - Explain, using relevant examples, how to implement sessions,
+ 	and the legal implications of doing this
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+ 	7 - Compare the express strategy toward (server side) templating with the one
+ 	you used with Java on 2nd semester
+ —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	8 - Explain, using relevant examples, your strategy for implementing a REST-API with
+	Node/Express and show how you can “test” all the four CRUD operations programmatically
+	using for example the Request package
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	9 - Explain, using relevant examples, about testing JavaScript code, relevant packages
+	(Mocha etc.) and how to test asynchronous code
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+	10 - Explain, using relevant examples, different ways to mock out databases, HTTP-request etc.
+—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+ */
